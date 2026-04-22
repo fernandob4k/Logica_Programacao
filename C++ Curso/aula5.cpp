68f72bef0a8ed1f0309e370ad76af7eea64591cf
Fernando Antunes <fernando.anascimento@live.com>
2026-04-21 22:33:39 -0300

    teste

--- C++ Curso/aula5.cpp
+++ C++ Curso/aula5.cpp
@@ -5,8 +5,7 @@ using namespace std;
 #define pi 3.1415
 #define canal cout << "canal fessor bruno"
 
-int main()
-{
+int main() {
     int vidas = 23, tiros = 500, life = 100;
 
     cout << pi << endl;

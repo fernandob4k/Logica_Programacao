498f6d71eec7f16182ff8b2b56acea51c445a73f
Fernando Antunes <fernando.anascimento@live.com>
2026-04-23 23:10:45 -0300

    update aula 6

--- C++ Curso/aula6.cpp
+++ C++ Curso/aula6.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int n1, n2; // Variáveis globais
 
-    
+int main()
+{
+    // operadores
 
-    return 0 ;
+    int n3, n4; // variáveis locais
+    int res, res2;
+    float div, divresto;
+
+    n1 = 11;
+    n2 = 3;
+    n3 = 4;
+    n4 = 2;
+
+    res = (n1 + n2 + n3 + n4) - 11;
+    res2 = (n1 + n2) * n4;
+    div = n1 / n2;
+    divresto = n1 % n2;
+    cout << "Soma de todas as variaveis: " << res << ":" << res2 << '\n';
+    cout << "Divisão = " << div << "O resto da divisão ou mod = " << divresto << '\n';
+    return 0;
 }

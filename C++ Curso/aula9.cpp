d0ea57ac79a2238e0e0cecdd4862fc200fc85fa8
Fernando Antunes <fernando.anascimento@live.com>
2026-04-28 17:07:09 -0300

    aula9

---
+++ C++ Curso/aula9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num = 514;
+
+    if (num < 5 ){
+        cout << "num é menor que 5 " << '\n';
+    } else if  (num == 5)
+    {
+        cout << "num é igual a 5 " << 'n' ;
+    } else {
+        cout << "10 é maior que  5 " << '\n';
+    }
+
+    cout << "Fim do programa" << '\n';
+    return 0;
+}

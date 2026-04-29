fd42d6eedaa23636269e26d411fa7c0ce7de5442
Fernando Antunes <fernando.anascimento@live.com>
2026-04-28 23:04:01 -0300

    switch aula 13

--- C++ Curso/aula12.cpp
+++ C++ Curso/aula12.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main () {
 
-    //expressão ternaria = condição ? expressão1 : expressão2
+    /*expressão ternaria = condição ? expressão1 : expressão2
 
     int n1 , n2, grade ;
     string  result ;
+    
 
     cout << "Enter the first grade: " ;
     cin >> n1 ;
@@ -18,8 +19,18 @@ int main () {
 
     (grade >= 60) ? result = "Approved" : result = "Reproved" ;
 
-    cout << "The student's test score is " << grade << " he is " << result << '\n';
+    cout << "The student's test score is " << grade << " he is " << result << '\n';*/
+    
+    int n1 , x;
 
+    x = 5;
+
+    cout << "Ente a value for n1:";
+    cin >> n1 ;
+
+    (n1 >= 10 ) ? x++ : x--;
+    cout << "the value of x is " << x << '\n';
+ 
     return 0 ;
 
 }

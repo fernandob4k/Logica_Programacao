167cd149e87cff235207be22c4333595794d3f3b
Fernando Antunes <fernando.anascimento@live.com>
2026-04-28 21:58:15 -0300

    Update aula11.cpp

--- C++ Curso/aula11.cpp
+++ C++ Curso/aula11.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream> 
 using namespace std;
 
@@ -14,4 +15,36 @@ int main ( ) {
     }
 
     return 0;
+=======
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num, cine;
+
+    num = 5;
+
+    if (num < 3 || num > 8)
+    {
+        cout << "Valor aceito" << endl;
+    }
+    else
+    {
+        cout << "Valor negado" << endl;
+    }
+
+    cine = 0;
+
+    if (cine == 1)
+    {
+        cout << "vamos ao cineme ";
+    }
+    else
+    {
+        cout << "vamos a praia ";
+    }
+>>>>>>> 2d5b6acff9761dcfc57f9d977c983a1807be9e63
 }
